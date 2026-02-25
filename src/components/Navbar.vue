3264a824059069e094b36e37e030ec73fb7807b4
Kelvin Junior <105466232+kelvinjunior77@users.noreply.github.com>
2026-02-26 00:34:38 +0100

    add link home

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -6,7 +6,7 @@ import { ref, onMounted, onBeforeUnmount } from 'vue'
 const activeSection = ref('')
 
 // lista de IDs das suas seções
-const sectionIds = ['hero', 'sobre', 'habilidades', 'projetos', 'contato', 'blogue']
+const sectionIds = ['home','hero', 'sobre', 'habilidades', 'projetos', 'contato', 'blogue']
 
 // Variável para guardar nosso "observador"
 let observer = null
@@ -69,6 +69,12 @@ onBeforeUnmount(() => {
       <ul class="menu menu-horizontal px-1 font-medium space-x-2">
 
         <li>
+          <a href="#home" class="border-b-4 pb-1 transition-colors duration-300"
+            :class="activeSection === 'home' ? 'border-success text-success' : 'border-transparent hover:border-error/40'">
+            Home
+          </a>
+        </li>
+        <li>
           <a href="#sobre" class="border-b-4 pb-1 transition-colors duration-300"
             :class="activeSection === 'sobre' ? 'border-success text-success' : 'border-transparent hover:border-error/40'">
             Sobre
@@ -124,26 +130,10 @@ onBeforeUnmount(() => {
           <li><a href="#">🇺🇸 English (EN)</a></li>
           <li><a href="#">🇫🇷 Français (FR)</a></li>
         </ul>
-      </div>---->
+      </div>
 
-      <a href="https://wa.me/+33753282632" target="_blank" class="hidden mr-3 ml-1 md:flex">
-
-        <svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" fill="currentColor" class="bi bi-whatsapp"
-          viewBox="0 0 16 16">
-          <path
-            d="M13.601 2.326A7.85 7.85 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.9 7.9 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.9 7.9 0 0 0 13.6 2.326zM7.994 14.521a6.6 6.6 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.56 6.56 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592m3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.73.73 0 0 0-.529.247c-.182.198-.691.677-.691 1.654s.71 1.916.81 2.049c.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232" />
-        </svg>
-
-      </a>
-
-      <a href="https://github.com/kelvinjunior77" target="_blank" class="hidden md:flex ml-0">
-        <svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" fill="currentColor" class="bi bi-github"
-          viewBox="0 0 16 16">
-          <path
-            d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8" />
-        </svg>
-      </a>
-      <a href="#contato" class="btn btn-success m-1 hidden md:flex">Contrate-me</a>
+      <a href="#contato" class="btn btn-success m-1 hidden md:flex">Contrate-me</a>---->
+      <a href="#contato" class="btn btn-success m-1 rounded-full hidden md:flex hover:text-white">Download CV</a>
 
       <div class="dropdown dropdown-end lg:hidden">
         <label tabindex="0" class="btn btn-ghost btn-circle">
@@ -155,6 +145,7 @@ onBeforeUnmount(() => {
         <ul tabindex="0" class="menu menu-md dropdown-content mt-3 z-[1] p-4 shadow bg-base-100 rounded-box w-64">
 
           <div class="flex justify-center space-x-6 py-2 mb-3 border-b border-base-200">
+          
             <a href="https://github.com/kelvinjunior77" target="_blank" class="text-base-content hover:text-primary">
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-github"
                 viewBox="0 0 16 16">
@@ -171,7 +162,7 @@ onBeforeUnmount(() => {
               </svg>
             </a>
 
-            <a href="https://www.instagram.com/kelvinjunior442/" target="_blank"
+            <a href="https://www.instagram.com/https://www.instagram.com/kevinndeke6/"  target="_blank"
               class="text-base-content hover:text-error">
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-instagram"
                 viewBox="0 0 16 16">
@@ -181,10 +172,13 @@ onBeforeUnmount(() => {
             </a>
           </div>
 
+          <li><a href="#home">Home</a></li>
           <li><a href="#sobre">Sobre</a></li>
           <li><a href="#habilidades">Habilidades</a></li>
           <li><a href="#projetos">Projetos</a></li>
           <li><a href="#contato">Contato</a></li>
+
+          <a href="#" class="btn btn-success w-full mt-2">Donwload CV</a>
         </ul>
       </div>
     </div>
