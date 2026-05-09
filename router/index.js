098bfd78a92b5c2ce5262d948351d58fb8ab9128
Kelvin Junior <105466232+kelvinjunior77@users.noreply.github.com>
2026-05-09 17:27:02 +0200

    add new page, add funcao de controlar o comportamente do scroll

--- router/index.js
+++ router/index.js
@@ -1,8 +1,9 @@
-import { createRouter, createWebHistory, createWebHashHistory} from "vue-router";
+import { createRouter, createWebHistory, createWebHashHistory } from "vue-router";
 import Home from "../pages/Home.vue";
 //const lazyLoad = (view) => import(`../pages/${view}.vue`);
 import Blogue from "../pages/Blogue.vue";
 import Post_1 from "../pages/posts/Post_1.vue";
+import Post_2 from "../pages/posts/Post_2.vue";
 
 const routes = [
     {
@@ -11,24 +12,42 @@ const routes = [
         component: Home,
     },
 
-     {
+    {
         path: "/blogue",
         name: "Blogue",
         //component: lazyLoad("Blogue"),
         component: Blogue
-     },
-     {
+    },
+    {
         path: "/blogue/post-1",
         name: "Post_1",
         component: Post_1
-     }
+    },
+    {
+        path: "/blogue/post-2",
+        name: "Post_2",
+        //component: lazyLoad("Post_2"),
+        component: Post_2
+    }
 
 ];
 
+// Esta função controla o comportamento do scroll
 const router = createRouter({
     history: createWebHistory(),
-    routes,
+    routes: [ ...routes ],
+
+    scrollBehavior(to, from, savedPosition) {
+        
+        if (savedPosition) {
+            return savedPosition;
+        } else {
+            
+            return { top: 0, behavior: 'smooth' }; // 'smooth' faz um deslize suave
+        }
+    },
 });
+
 
 export default router;
 
