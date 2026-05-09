62c24f3d3a0394cc4e8c0ccc0468dc99c3bfe36e
Kelvin Junior <105466232+kelvinjunior77@users.noreply.github.com>
2026-05-09 17:28:03 +0200

    create Layout Blogue

---
+++ src/components/blog/Layout.vue
@@ -0,0 +1,32 @@
+<script setup>
+import NavbarBlogue from '../NavbarBlogue.vue';
+import CardBlogue from '../cards/CardBlogue.vue';
+import FooterBlogue from './FooterBlogue.vue';
+</script>
+
+<template>
+    <div class="font-sans text-ink antialiased">
+        <NavbarBlogue />
+
+        <main class="pt-14">
+            <article class="max-w-3xl mx-auto px-6 py-16 mt-10">
+
+                <slot name="shre"></slot>
+
+                <!-- Body - Texto -->
+
+                <slot >
+                    
+                    <!--Post-->
+
+                </slot>
+               
+          
+            </article>
+            
+            <CardBlogue />
+        </main>
+    </div>
+
+    <FooterBlogue />
+</template>
